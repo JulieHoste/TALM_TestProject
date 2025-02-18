965a466828c9ca5ce6b8815e15c774bf6d93d9ff
hostej <hostej@DCICND1273F3H.gsinet.be>
2025-02-18 17:07:17 +0100

    TC appointment

--- Scripts/TC - Appointment/Script1739894506000.groovy
+++ Scripts/TC - Appointment/Script1739894506000.groovy
@@ -17,19 +17,22 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('')
+WebUI.callTestCase(findTestCase('TC - Login'), [:], FailureHandling.STOP_ON_FAILURE)
 
-WebUI.openBrowser('')
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/label_Medicaid'))
 
-WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/')
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/span_Visit Date (Required)_glyphicon glyphi_cada34'))
 
-WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/h1_CURA Healthcare Service'))
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/td_19'))
 
-WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/a_Make Appointment'))
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/td_20'))
 
-WebUI.setText(findTestObject('Object Repository/Page_CURA Healthcare Service/input_Username_username'), 'John Doe')
+WebUI.setText(findTestObject('Object Repository/Page_CURA Healthcare Service/textarea_Comment_comment'), 'test')
 
-WebUI.setEncryptedText(findTestObject('Object Repository/Page_CURA Healthcare Service/input_Password_password'), 'g3/DOGG74jC3Flrr3yH+3D/yKbOqqUNM')
+WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/button_Book Appointment'))
 
-WebUI.click(findTestObject('Object Repository/Page_CURA Healthcare Service/button_Login'))
+WebUI.navigateToUrl('https://katalon-demo-cura.herokuapp.com/appointment.php#summary')
+
+WebUI.verifyElementPresent(findTestObject('Object Repository/Page_CURA Healthcare Service/h2_Appointment Confirmation'), 
+    30)
 
